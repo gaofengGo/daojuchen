78e91fa729a8a1f4796dfa87b93f93c815fd42fc
fsafafaf <234226015@qq.com>
2017-12-03 22:04:02 +0800

    完成了gift页面

--- daojuchen/pages/gift/gift.js
+++ daojuchen/pages/gift/gift.js
@@ -5,9 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      jobs:[
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/drqy-266.jpg',
+          title:'达人契约 (3天)',
+          small:'50',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/czqy-266.jpg',
+        title:'成长之契约 (3天)',
+        small:'75',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/gjdhq-266.jpg',
+        title:'高级装扮兑换卷 (无期限)',
+        small:'288',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/pjtzx.png',
+        title:'装备品级调整箱 (15个)',
+        small:'15',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/266-42.png',
+        title:'复活币',
+        small:'10',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/266-10000388.png',
+        title:'晶之契约 (3天)',
+        small:'100',
+        },
+        { image:'http://ossweb-img.qq.com/images/gameshop/dnf/images/thumb/266-2660671.png',
+        title:'闪亮的雷米援助 (15个)',
+        small:'50',
+        },
+     ],
   },
-
+  logbtn: function() {
+    wx.showToast({
+      title:'兑换中',
+      icon:'loading',
+      duration:1500,
+    }) 
+    setTimeout(function(){
+      wx.showToast({
+      title:'兑换成功',
+      icon:'success',
+      duration:1000,
+    })},1500)  
+},
   /**
    * 生命周期函数--监听页面加载
    */
